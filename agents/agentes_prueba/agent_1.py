49d98564e755d06facface1ead719b91288455bf
Evan Gil Cortés <evaanngc@gmail.com>
2025-03-22 14:17:17 +0100

    feat: :sparkles: Add logger and use cases

--- agents/agentes_prueba/agent_1.py
+++ agents/agentes_prueba/agent_1.py
@@ -6,6 +6,8 @@ from langchain.chains import LLMChain
 from langchain_openai import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 
+from core.logger import logger
+
 # Load environment variables
 load_dotenv()
 
@@ -17,33 +19,35 @@ llm = ChatGoogleGenerativeAI(
     temperature=0.5
 )
 
-# # Basic example of chat 
-# response = llm.invoke("¿Cuáles son los principales beneficios de usar LangChain?")
-# print(response.content)
+logger.info("Model loaded successfully")
 
-# # Create a prompt template
-# template = """Actúa como un experto en {tema}. Proporciona una explicación detallada sobre: {concepto}."""
+# Basic example of chat 
+response = llm.invoke("¿Cuáles son los principales beneficios de usar LangChain?")
+print(response.content)
 
-# # Create a prompt
-# prompt = PromptTemplate(
-#     input_variables=["tema", "concepto"],
-#     template=template
-# )
+# Create a prompt template
+template = """Actúa como un experto en {tema}. Proporciona una explicación detallada sobre: {concepto}."""
 
-# # Create a chain
-# chain = prompt | llm
+# Create a prompt
+prompt = PromptTemplate(
+    input_variables=["tema", "concepto"],
+    template=template
+)
 
-# # Execute the chain
-# response = chain.invoke({
-#     "tema": "ingeligencia artificial", 
-#     "concepto": "redes neuronales convolucionales"
-# })
+# Create a chain
+chain = prompt | llm
 
-# print(response.content)
+# Execute the chain
+response = chain.invoke({
+    "tema": "ingeligencia artificial", 
+    "concepto": "redes neuronales convolucionales"
+})
+
+print(response.content)
 
 # Configure the memory -> we set the memory_key to chat_history to use this variable in the prompt. It stores the conversation history.
 
-# memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 # Create a prompt template
 template = """La siguiente es una conversacion amigable entre un humano y un asistente AI.
@@ -52,22 +56,22 @@ Historial de la conversacion:
 Humano: {input}
 Asistente:"""
 
-# # Create a prompt
-# prompt = PromptTemplate(
-#     input_variables=["chat_history", "input"],
-#     template=template
-# )
+# Create a prompt
+prompt = PromptTemplate(
+    input_variables=["chat_history", "input"],
+    template=template
+)
 
-# conversation = LLMChain(
-#     llm=llm,
-#     memory=memory,
-#     prompt=prompt,
-#     verbose=True
-# )
+conversation = LLMChain(
+    llm=llm,
+    memory=memory,
+    prompt=prompt,
+    verbose=True
+)
 
-# print(conversation.predict(input="Que pais ha ganado mas mundiales de futbol?"))
-# print(conversation.predict(input="Nombrame 10 jugadores historicos de esa seleccion"))
-# print(conversation.predict(input="De que color era su camiseta?"))
+print(conversation.predict(input="Que pais ha ganado mas mundiales de futbol?"))
+print(conversation.predict(input="Nombrame 10 jugadores historicos de esa seleccion"))
+print(conversation.predict(input="De que color era su camiseta?"))
 
 # Sentiment analysis
 sentiment_template = """Analiza el sentimiento del siguiente texto y clasif'ícalo como positivo, negativo o neutral. Proporciona también una explicación de tu análisis.
@@ -83,6 +87,8 @@ sentiment_prompt = PromptTemplate(
     template=sentiment_template
 )
 
+logger.info("✅ Prompt template created successfully")
+
 # Create the chain -> LLMChain is deprecated so we use the | operator to chain the prompts using the order: prompt | llm where prompt is the prompt template and llm is the language model. The prompt has to be the first element in the chain.
 sentiment_chain = sentiment_prompt | llm
 
@@ -93,5 +99,7 @@ example_text = "El nuevo restaurante supero todas mis expectativas. La comida es
 response = sentiment_chain.invoke({
     "texto": example_text
 })
+
+logger.info("✅ Chain executed successfully")
 
 print(response.content)
