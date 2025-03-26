f663d34b2490af375787f887b5153b70eba972fc
Evan Gil Cortés <evaanngc@gmail.com>
2025-03-26 01:41:50 +0100

    refactor: 🗑️ Remove old project structure files

--- core/constants.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
-
-GENAI_API_KEY = os.getenv("GENAI_API_KEY")
-LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
