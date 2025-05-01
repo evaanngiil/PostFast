9fe1c25c4a2215c6a55f409e94d06def0ce71994
Evan Gil Cortés <evaanngc@gmail.com>
2025-05-01 22:16:14 +0200

    fix: 🐛 Correct timezone configuration in Celery app from UTC+1 to UTC

--- src/celery_app.py
+++ src/celery_app.py
@@ -15,7 +15,7 @@ celery_app.conf.update(
     task_serializer="json",
     accept_content=["json"],
     result_serializer="json",
-    timezone="UTC+1",
+    timezone="UTC",
     enable_utc=True,
     task_track_started=True,
 )
