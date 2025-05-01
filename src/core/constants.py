0ca7f67c9eddc689cd6c3edb2a3612ef02cd2418
Evan Gil Cortés <evaanngc@gmail.com>
2025-05-01 22:18:05 +0200

    fix: 🐛 Remove unused Facebook API keys and related constants

--- src/core/constants.py
+++ src/core/constants.py
@@ -7,15 +7,12 @@ load_dotenv()
 GENAI_API_KEY = os.getenv("GENAI_API_KEY")
 LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
 
-FB_CLIENT_ID = os.getenv("FB_CLIENT_ID")
-FB_CLIENT_SECRET = os.getenv("FB_CLIENT_SECRET")
 LI_CLIENT_ID = os.getenv("LI_CLIENT_ID")
 LI_CLIENT_SECRET = os.getenv("LI_CLIENT_SECRET")
 
 BASE_URL = os.getenv("BASE_URL")
 FASTAPI_URL = os.getenv("FASTAPI_URL")
 
-FB_REDIRECT_URI = f"{FASTAPI_URL}/auth/callback/facebook"
 LI_REDIRECT_URI = f"{FASTAPI_URL}/auth/callback/linkedin"
 X_REDIRECT_URI = f"{FASTAPI_URL}/auth/callback/x"
 
@@ -25,6 +22,9 @@ CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # API Constants 
-FB_GRAPH_URL = "https://graph.facebook.com/v18.0"
 # LI_API_URL = "https://api.linkedin.com/rest"
 LI_API_URL = "https://api.linkedin.com/v2"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    print("CRITICAL: DATABASE_URL environment variable not set.")
